a37c33ac13012953b436908e50e9b5dc3e84d7b9
putra-evans <putraevans001@gmail.com>
2020-09-21 02:57:52 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Tutorial Google Map - Petani Kode</title>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCmwLJoUkcw1xvSFNtb1-Xt8908vYQGRv4&callback=initMap&libraries=&v=weekly"></script>
+  <script>
+    // variabel global marker
+    var marker;
+
+    function taruhMarker(peta, posisiTitik) {
+
+      if (marker) {
+        // pindahkan marker
+        marker.setPosition(posisiTitik);
+      } else {
+        // buat marker baru
+        marker = new google.maps.Marker({
+          position: posisiTitik,
+          map: peta
+        });
+      }
+
+    }
+
+    function initialize() {
+      var propertiPeta = {
+        center: new google.maps.LatLng(-0.929661, 100.3772193),
+        zoom: 9,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+
+      var peta = new google.maps.Map(document.getElementById("googleMap"), propertiPeta);
+
+      // even listner ketika peta diklik
+      google.maps.event.addListener(peta, 'click', function(event) {
+        taruhMarker(this, event.latLng);
+      });
+
+    }
+
+
+    // event jendela di-load  
+    google.maps.event.addDomListener(window, 'load', initialize);
+  </script>
+
+</head>
+
+<body>
+
+  <div align=center id="googleMap" style="width:70%;height:550px;"></div>
+
+</body>
+
+</html>
